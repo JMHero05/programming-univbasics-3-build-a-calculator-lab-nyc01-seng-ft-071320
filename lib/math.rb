c0d82a37860bff61e5f77d775c7bd835fcb9b3a7
Jake Mills <jake.mills.05@gmail.com>
2020-06-29 21:26:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum  = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  sub = num1 - num2
+  sub
 end
 
 def division(num1, num2)
-
+  dif = num1 / num2
+  dif
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  mult
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  mod
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
